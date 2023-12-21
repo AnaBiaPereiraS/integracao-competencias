93ff77efe0868970d613a788cff99705695f34d6
Ana Beatriz <ana.b.sousa22@aluno.senai.br>
2023-12-20 21:38:03 -0300

    criar quiz com checkbox

---
+++ js/questions.js
@@ -0,0 +1,8 @@
+export default [
+    {
+        question: "",
+        answers: [
+            { option: "Abuja", correct: true},
+            { option: "Cidade do Cabo", correct: false},
+            { option: "Luanda", correct: false},
+        ],
