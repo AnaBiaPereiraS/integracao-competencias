4e6ec9ba2e98f5d45b07969d909c7e3d8b727aa7
Ana Beatriz <ana.b.sousa22@aluno.senai.br>
2023-12-22 21:18:18 -0300

    criação da api

--- js/questions.js
+++
@@ -1,8 +0,0 @@
-export default [
-    {
-        question: "",
-        answers: [
-            { option: "Abuja", correct: true},
-            { option: "Cidade do Cabo", correct: false},
-            { option: "Luanda", correct: false},
-        ],
