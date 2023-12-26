6d0beddea039122a8e901beaeffb4cf76adb46cc
Ana Beatriz Pereira Sousa <148508550+AnaBiaPereiraS@users.noreply.github.com>
2023-12-26 18:14:42 -0300

    criação do main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,39 @@
 import {getAllQuestions} from './question.service.js';
 
+var mock = [
+    {
+    questao: "1- Quantas linhas tem no logo do serviço de streaming de música Spotify?",
+    respostas: {
+        a: '2 linhas',
+        b: '3 linhas',
+        c: '4 linhas',
+        d: '5 linhas'
+    },
+    'respostaCorreta': 'b'
+    },
+
+    {
+    questao: "2- O sushi é um prato original de qual país?",
+    respostas: {
+        a: 'Tailândia',
+        b: 'China',
+        c: 'Japão',
+        d: 'Corêia do Sul'
+    },
+    'respostaCorreta': 'c'
+     },
+    
+     {
+    questao: "3. Qual é o sobrenome mais comum no Brasil",
+    respostas: {
+        a: 'Silva',
+        b: 'Santos',
+        c: 'Sousa',
+        d: 'Oliveira'
+    },
+    'respostaCorreta': 'a'
+    },
+]
 
 
 
