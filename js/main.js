919efa7066da581d66cd5df308838481f5017085
Ana Beatriz <ana.b.sousa22@aluno.senai.br>
2023-12-27 21:43:41 -0300

    main.js corrigido, fazer apenas alteraçoes no css

--- js/main.js
+++ js/main.js
@@ -1,105 +1,75 @@
-import {getAllQuestions} from './question.service.js';
-
-var mock = [
-    {
-    questao: "1- Quantas linhas tem no logo do serviço de streaming de música Spotify?",
-    respostas: {
-        a: '2 linhas',
-        b: '3 linhas',
-        c: '4 linhas',
-        d: '5 linhas'
-    },
-    'respostaCorreta': 'b'
-    },
-
-    {
-    questao: "2- O sushi é um prato original de qual país?",
-    respostas: {
-        a: 'Tailândia',
-        b: 'China',
-        c: 'Japão',
-        d: 'Corêia do Sul'
-    },
-    'respostaCorreta': 'c'
-     },
-    
-     {
-    questao: "3. Qual é o sobrenome mais comum no Brasil",
-    respostas: {
-        a: 'Silva',
-        b: 'Santos',
-        c: 'Sousa',
-        d: 'Oliveira'
-    },
-    'respostaCorreta': 'a'
-    },
-];
+import { getAllQuestions } from './question.service.js';
 
 window.onload = () => {
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var submitButton = document.getElementById('submit');
+    var quizContainer = document.getElementById('quiz');
+    var resultsContainer = document.getElementById('results');
+    var submitButton = document.getElementById('submit');
 
-loadQuestions().then (questions => {console.log(questions)
-    generateQuiz(mock, quizContainer, resultsContainer, submitButton);
-});
+    loadQuestions().then(questions => {
+        generateQuiz(questions, quizContainer, resultsContainer, submitButton);
+    });
 }
 
-const loadQuestions=() => {
-return getAllQuestions().then(questions => {
-    console.log('questions', questions)
-    return questions;
-})
+const loadQuestions = () => {
+    return getAllQuestions().then(questions => {
+        return questions;
+    })
 }
-function generateQuiz(questions, quizContainer, resultsContainer,submitButton){
 
-    function showQuestions(questions,quizContainer){
-var output = [];
-var respostas;
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
 
-for(var i=0; i<questions.length; i++){
-respostas = [];
+    function showQuestions(questions, quizContainer) {
+        var output = [];
+        var respostas;
 
-for(var letter in questions[i].respostas){
-    respostas.push(
-        '<label>'
-        + '<input type="radio" name="questao'+i+'" value="'+letter+'">'
-        + questions [i].respostas[letter]
-        +'</label>'
-    );
-}
-output.push(
-    '<div class="questao">' + questions [i].questao + '</div>'
-    + '<div class = "respostas">'+ respostas.join('') + '</div>'
-);
+        for (var i = 0; i < questions.length; i++) {
+            respostas = [];
 
-}
+            for (var letter in questions[i].respostas) {
+                respostas.push(
+                    '<label>'
+                    + '<input type="radio" name="questao' + i + '" value="' + letter + '">'
+                    + questions[i].respostas[letter]
+                    + '</label>'
+                );
+            }
+            output.push(
+                '<div class="questao">' + questions[i].questao + '</div>'
+                + '<div class = "respostas">' + respostas.join('') + '</div>'
+            );
+
+        }
+        quizContainer.innerHTML = output.join('');
     }
-    quizContainer.innerHTML = output.join('');
-}
 
-function showResults(questions,quizContainer,resultsContainer){
-var answerContainers = quizContainer.querySelectorAll('.respostas');
-var userAnswer='';
-var numCorrect=0;
+    function showResults(questions, quizContainer, resultsContainer) {
+        var answerContainers = quizContainer.querySelectorAll('.respostas');
+        var userAnswer = '';
+        var numCorrect = 0;
 
-userAnswer = (answerContainers[i].querySelector('input[name=questao'+i+']:checked') || {}).value;
+        for (var i = 0; i < questions.length; i++) {
 
-if (userAnswer===questios[i].respostaCorreta){
+            userAnswer = (answerContainers[i].querySelector('input[name=questao' + i + ']:checked') || {}).value;
 
-numCorrect++;
+            if (userAnswer === questions[i].respostaCorreta) {
 
-answerContainers[i].style.color='lightgreen';
-}
+                numCorrect++;
 
-else {
-    answerContainers[i].style.color = 'red';
-}
-resultsContainer.innerHTML = ` Acertou ${numCorrect} das ${questions.length} questoes.`;
+                answerContainers[i].style.color = 'lightgreen';
+            }
 
-showQuestions(questions, quizContainer);
+            else {
+                answerContainers[i].style.color = 'red';
+            }
 
-submitButton.onclick = function(){
-showResults (questions, quizContainer,resultsContainer);
-}
+            resultsContainer.innerHTML = ` Acertou ${numCorrect} das ${questions.length} questoes.`;
+
+        }
+    }
+
+    showQuestions(questions, quizContainer);
+
+    submitButton.onclick = function () {
+        showResults(questions, quizContainer, resultsContainer);
+    }
 }
